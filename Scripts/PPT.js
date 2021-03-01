3a2ceb3617877b1f73400cd408281ecaee2dc581
FRR95 <jbau58@gmail.com>
2021-03-01 17:50:08 +0100

    Revisión

--- Scripts/PPT.js
+++ Scripts/PPT.js
@@ -1,4 +1,4 @@
-var app = angular.module('appppt', []);
+/* var app = angular.module('appppt', []);
 
 var a = "Piedra";
 var b = "Papel";
@@ -38,6 +38,39 @@ $scope.Func_PPT = function() {
     $scope.ShowResult = !$scope.ShowResult;
   } 
 });
+*/
+
+var a = "Piedra";
+var b = "Papel";
+var c = "Tijera";
+var MachineAnswer = window['abc'.charAt(Math.floor(Math.random()*3))];
+
+
+var PPTApp = angular.module('PPTApp', []);
+    PPTApp.controller('PPTController', function($scope) {
+    
+      $scope.OptionsPlayer = ["Piedra", "Papel", "Tijera"];
+      
+     
+      $scope.ShowResult = false;
+      $scope.PlayerSelection = $scope.OptionsPlayer[0];
+      $scope.Func_PPT = function() {
+        $(document).ready(function(){
+
+          
+   
+ 
+          $('#MachineResult').html("La maquina ha elegido: " +MachineAnswer);
+         
+         
+        });
+       
+      $scope.ShowResult = !$scope.ShowResult;
+  
+
+    
+    }
+    });
 
 
 
