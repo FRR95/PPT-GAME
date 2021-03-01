f1bfed8b2bb30f64c271bef625d5e8521817411f
FRR95 <jbau58@gmail.com>
2021-03-01 20:01:55 +0100

    Revisión

--- Scripts/PPT.js
+++ Scripts/PPT.js
@@ -40,31 +40,37 @@ $scope.Func_PPT = function() {
 });
 */
 
-var a = "Piedra";
-var b = "Papel";
-var c = "Tijera";
-var MachineAnswer = window['abc'.charAt(Math.floor(Math.random()*3))];
+
+
 
 
 var PPTApp = angular.module('PPTApp', []);
     PPTApp.controller('PPTController', function($scope) {
     
       $scope.OptionsPlayer = ["Piedra", "Papel", "Tijera"];
+      $scope.OptionsMachine = [
+        {
+            value: "Piedra"
+        },
+        {
+            value: "Papel"
+        },
+        {
+          value: "Tijera"
+        },
+
+    ];
+      
+      
       
      
       $scope.ShowResult = false;
       $scope.PlayerSelection = $scope.OptionsPlayer[0];
+      $scope.MachineSelection = $scope.OptionsMachine[0];
+      
       $scope.Func_PPT = function() {
-        $(document).ready(function(){
-
-          
-   
- 
-          $('#MachineResult').html("La maquina ha elegido: " +MachineAnswer);
-         
-         
-        });
        
+      $scope.randomQuote = [Math.floor(Math.random() * $scope.OptionsMachine.length)]; 
       $scope.ShowResult = !$scope.ShowResult;
   
 
