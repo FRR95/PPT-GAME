ca6b96a74377ce7562d8a099925ba5bf0c2c7cfd
FRR95 <jbau58@gmail.com>
2021-03-02 18:48:45 +0100

    Revisión

--- Scripts/PPT.js
+++ Scripts/PPT.js
@@ -1,81 +1,32 @@
-/* var app = angular.module('appppt', []);
-
-var a = "Piedra";
-var b = "Papel";
-var c = "Tijera";
-var MachineAnswer = window['abc'.charAt(Math.floor(Math.random()*3))];
-
-
-app.controller('pptcontroller', function($scope) {
-$scope.names = ["Piedra", "Papel", "Tijeras"];
-
-$scope.ShowResult = false;
-
-
-$scope.Func_PPT = function() {
-  $scope.getRandomIndex = function(length){
-    return Math.floor(Math.random() * length);
-}
-    $scope.quotes = [
-        {
-            value: "q1"
-        },
-        {
-            value: "q2"
-        },
-
-    ];
-
-  $(document).ready(function(){
-
-  
-   
- 
-    $('#MachineResult').html("La maquina ha elegido: " +MachineAnswer);
-   
-   
-  });
-    $scope.ShowResult = !$scope.ShowResult;
-  } 
-});
-*/
-
-
-
-
-
 var PPTApp = angular.module('PPTApp', []);
+
+
+
+
+
+
     PPTApp.controller('PPTController', function($scope) {
     
-      $scope.OptionsPlayer = ["Piedra", "Papel", "Tijera"];
-      $scope.OptionsMachine = [
-        {
-            value: "Piedra"
-        },
-        {
-            value: "Papel"
-        },
-        {
-          value: "Tijera"
-        },
-
-    ];
-      
+      $scope.OptionsPlayer1 = ["Piedra", "Papel", "Tijera"];
+      $scope.OptionsPlayer2 = ["Piedra", "Papel", "Tijera"];
+     
+     
       
       
      
       $scope.ShowResult = false;
-      $scope.PlayerSelection = $scope.OptionsPlayer[0];
-      $scope.MachineSelection = $scope.OptionsMachine[0];
+      $scope.Player1Selection = $scope.OptionsPlayer1[0];
+      $scope.Player2Selection = $scope.OptionsPlayer2[0];
+     
+     
+     
       
       $scope.Func_PPT = function() {
        
-      $scope.randomQuote = [Math.floor(Math.random() * $scope.OptionsMachine.length)]; 
-      $scope.ShowResult = !$scope.ShowResult;
-  
-
-    
-    }
+        $scope.ShowResult = !$scope.ShowResult;
+        
+      
+      }
     });
 
 
