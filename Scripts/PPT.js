5784489192048685e8707d581e0f365e4621a4b8
FRR95 <jbau58@gmail.com>
2021-03-08 14:40:29 +0100

    Revisión

--- Scripts/PPT.js
+++ Scripts/PPT.js
@@ -14,11 +14,46 @@ var PPTApp = angular.module('PPTApp', []);
       
       
      
-      /*$scope.ShowResult = false;*/
+      $scope.ShowResult = false;
       $scope.Player1Selection = $scope.OptionsPlayer1[0];
       $scope.Player2Selection = $scope.OptionsPlayer2[0];
      
-     
+      $scope.FunctionResult = function () {
+        $scope.ShowResult = true;
+        
+        
+       if($scope.Player1Selection=='Piedra' && $scope.Player2Selection=='Tijera'){
+        $scope.Player1Wins = $scope.Player1Wins + 1;
+      }
+      if($scope.Player1Selection=='Papel' && $scope.Player2Selection=='Piedra'){
+        $scope.Player1Wins = $scope.Player1Wins + 1;
+      
+       }
+       if($scope.Player1Selection=='Tijera' && $scope.Player2Selection=='Papel'){
+        $scope.Player1Wins = $scope.Player1Wins + 1;
+      
+       }
+
+       if($scope.Player1Selection=='Tijera' && $scope.Player2Selection=='Piedra'){
+        $scope.Player2Wins = $scope.Player2Wins + 1;
+      }
+      if($scope.Player1Selection=='Piedra' && $scope.Player2Selection=='Papel'){
+        $scope.Player2Wins = $scope.Player2Wins + 1;
+      
+       }
+       if($scope.Player1Selection=='Papel' && $scope.Player2Selection=='Tijera'){
+        $scope.Player2Wins = $scope.Player2Wins + 1;
+      
+       }
+
+      
+      
+      }
+      $scope.hideresult = function () {
+
+        $scope.ShowResult = false;
+    
+      }
      
       
      
