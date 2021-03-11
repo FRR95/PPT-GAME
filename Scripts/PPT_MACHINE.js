4f76172faaf54edc4c9964b4fe299cd323c07653
ikerkm <47975394+ikerkm@users.noreply.github.com>
2021-03-11 12:13:44 +0100

    Update PPT_MACHINE.js

--- Scripts/PPT_MACHINE.js
+++ Scripts/PPT_MACHINE.js
@@ -1,14 +1,8 @@
 var PPTApp = angular.module('PPTApp', []);
 
-
-
-
-
-
     PPTApp.controller('PPTController', function($scope) {
-    
       $scope.OptionsPlayer1 = ["Piedra", "Papel", "Tijera"];
-     
+     //TODO: Elimina codigo comentado
      /* $scope.MachineArray = [
         {
             value: "Piedra"
@@ -22,15 +16,9 @@ var PPTApp = angular.module('PPTApp', []);
 
     ];*/
     $scope.MachineArray = ["Piedra", "Papel", "Tijera"];
-     
-      
-      
-     
+
       $scope.ShowResult = false;
       $scope.Player1Selection = $scope.OptionsPlayer1[0];
-
-      
-     
       $scope.FunctionResult = function () {
         $scope.ShowResult = true;
         $scope.RandomMachineAnswer = $scope.MachineArray[Math.floor(Math.random() * $scope.MachineArray.length)];
@@ -41,19 +29,19 @@ var PPTApp = angular.module('PPTApp', []);
         $("#Result").html('Jugador 1 gana');
         });
       }
+          
       if($scope.Player1Selection=='Papel' && $scope.RandomMachineAnswer=='Piedra'){
         $scope.Player1Wins = $scope.Player1Wins + 1;
         $(document).ready(function(){
           $("#Result").html('Jugador 1 gana');
           });
-      
        }
+          
        if($scope.Player1Selection=='Tijera' && $scope.RandomMachineAnswer=='Papel'){
         $scope.Player1Wins = $scope.Player1Wins + 1;
         $(document).ready(function(){
           $("#Result").html('Jugador 1 gana');
           });
-      
        }
 
        if($scope.Player1Selection=='Tijera' && $scope.RandomMachineAnswer=='Piedra'){
@@ -62,42 +50,30 @@ var PPTApp = angular.module('PPTApp', []);
           $("#Result").html('La máquina gana');
           });
       }
+          
       if($scope.Player1Selection=='Piedra' && $scope.RandomMachineAnswer=='Papel'){
         $scope.MachineWins = $scope.MachineWins + 1;
         $(document).ready(function(){
           $("#Result").html('La máquina gana');
           });
-      
        }
+          
        if($scope.Player1Selection=='Papel' && $scope.RandomMachineAnswer=='Tijera'){
         $scope.MachineWins = $scope.MachineWins + 1;
         $(document).ready(function(){
           $("#Result").html('La máquina gana');
           });
-      
        }
 
        if($scope.Player1Selection==$scope.RandomMachineAnswer){
-       
         $(document).ready(function(){
           $("#Result").html('Empate');
           });
-      
        }
-      
-      
-      
       }
+        
       $scope.hideresult = function () {
-
         $scope.ShowResult = false;
-    
       }
-     
-      
-     
+    
     });
-
-
-
-
