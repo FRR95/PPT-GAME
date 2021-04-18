829ac8f1a58ffda57994a701e50124b01d2e93c7
FRR95 <jbau58@gmail.com>
2021-04-18 18:34:00 +0200

    Revisión 18/04/21

--- Main/stats.php
+++ Main/stats.php
@@ -1,26 +1,28 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://localhost/phpmyadmin/index.php");
 $conectar = mysqli_connect('localhost','fran','18Noviembre95', 'fran');
 
 
 if(!$conectar){
-    echo "No conectado";
+   echo "No conectado";
 }
 else{
 
 $playerwins = $_POST['playerwins'];
 $machinewins = $_POST['machinewins'];
 
-$sql = "INSERT INTO playerstats(playerwins,machinewins)values('$playerwins','$machinewins')";
+$sql = "INSERT INTO playerstats(playerwins,machinewins) values($playerwins,$machinewins)";
 
 $ejecutar = mysqli_query($conectar, $sql);
+
 }
 
 if(!$ejecutar){
-    echo "Hay algun error";
+   echo "Hay algun error";
 }
 else{
-    echo "Datos almacenados correctamente";
+   echo "Datos almacenados correctamente";
 }
+
+exit;
 ?>
