d39c815ece659e77a285f5d4bfb43b44716fc763
FRR95 <jbau58@gmail.com>
2021-04-24 18:53:11 +0200

    Revisión 24/04/21

--- Main/rankings.php
+++ Main/rankings.php
@@ -8,6 +8,13 @@
 <!DOCTYPE html>
 <html>
 <head>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>    
+
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>	
 <link rel="stylesheet" href="../Styles/CommonStyle.css">
 <img class="ImgIndex" src="../IMGS/PPT.png" alt="Girl in a jacket" width="512" height="512">
 <a href="Index.html" >Volver al menú principal</a>
@@ -17,31 +24,44 @@
 
 <br>
 
-	<table class="score_results">
+	<table id="table" class="score_results">
+	<thead>
 		<tr>
 		    <td>Resultado</td>
 			<td>Jugador</td>
 			<td>Máquina</td>
 			
 		</tr>
-
+	</thead>
 		<?php 
 		$sql="SELECT * FROM `playerstats1`";
 		$result=mysqli_query($conectar,$sql);
 
 		while($mostrar=mysqli_fetch_array($result)){
 		 ?>
-
-		<tr class="color_state">
+    <tbody>
+		<tr>
 		    <td><?php echo $mostrar['states'] ?></td>
 			<td><?php echo $mostrar['playerwins'] ?></td>
 			<td><?php echo $mostrar['machinewins'] ?></td>
 	
 		</tr>
+		</tbody>
 	<?php 
 	}
 	 ?>
+
 	</table>
 
+
 </body>
+<script>
+$(function() {
+  $('#table td:last-child:contains(5)').closest('tr').css('background-color', '#F84646');
+  $('#table td:nth-child(2):contains(5)').closest('tr').css('background-color', 'rgb(79, 155, 226)');
+  
+ 
+
+});	
+</script>
 </html>
